15624fcc87551310b15e2765031bf1ea7b6096df
syuhada-ramadhan <suhadahada18@gmail.com>
2021-11-26 09:29:45 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// value types
+// let hewanA = 12;
+// let hewanB = hewanA;
+
+// hewanB = hewanB + 2;
+
+// console.log("Hewan A =", hewanA);
+// console.log("Hewan B =", hewanB);
+
+// reference types
+let hewanA = {
+  umur: 12,
+};
+let hewanB = hewanA;
+
+hewanB.umur = 14;
+
+console.log("Hewan A =", hewanA);
+console.log("Hewan B =", hewanB);
